1e4c53d09dd72289a64412e89d830d3a3ebcd550
Alex Sea <seagullalex81@gmail.com>
2024-07-22 17:58:14 +0300

    add css task 2, linked flatpickr

---
+++ src/js/1-timer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let userSelectedDate = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
+};
